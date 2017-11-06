97f92939896d3ed1e8072752bcd69a6fe19135ab
kittipanP <kittipan.prasertsang@gmail.com>
2017-11-06 15:33:30 +0700

    added form Login to index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 	<head>
 		<meta charset="utf-8"
 		<title></title>
+		<link href="style.css" rel="stylesheet" type="text/css" >
 	</head>
 	<body>
-		<php?
-
-		?>
+		<div>
+			<form action="login.php" method="POST">
+				<label for="username">Username: </label>
+				<input type="text" name="username" required autofocus>
+				<label for="password">password: </label>
+				<input type="password" name="password" required>
+				<br>
+				<input type="submit" value="Log In">
+			</form>
+			<a href="frm_register.php">Register</a>
+		</div>
 	</body>
 </html>
